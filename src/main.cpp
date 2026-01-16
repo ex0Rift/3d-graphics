038305689039627fc8985032e5afe3170fe91534
Nate <daviesnathanael45@gmail.com>
2026-01-16 14:42:35 +0000

    shaders are written and are compiled in the program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include "settings.h"
 #include "inputs.hpp"
 
+const char *vertexShaderSource = "#version 330 core \n"
+    "layout(location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
 int main()
 {
     glfwInit();
@@ -21,9 +35,58 @@ int main()
         std::cout << "Failed to init GLAD" << std::endl;
         return -1;
     }
-
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
+    //setup shaders
+    //-------------
+
+    //vertex shader
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    //check if compilation was successful
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    //fragment shader
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader,1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    //check if compilation was successful
+    int successFrag;
+    char infoLogFrag[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &successFrag);
+    if (!successFrag)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLogFrag);
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLogFrag << std::endl;
+    }
+
+    //set up the buffers and vertex
+    float verticies[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+
+    // the mainloop where everything repeats every frame
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
